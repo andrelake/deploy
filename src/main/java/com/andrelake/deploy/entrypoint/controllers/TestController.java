38082769a654dfbc2a5aa4b764b4cd11488839b9
andreLake <andre.lake@hotmail.com>
2025-02-09 12:41:54 -0300

    create a product local

--- src/main/java/com/andrelake/deploy/entrypoint/controllers/TestController.java
+++ src/main/java/com/andrelake/deploy/entrypoint/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.andrelake.deploy.entrypoint.controllers;
 
+import com.andrelake.deploy.domain.entities.Product;
+import com.andrelake.deploy.entrypoint.repositories.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private final ProductRepository productRepository;
+
+    public TestController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping
     public String getTest() {
         return "test";
+    }
+
+    @GetMapping("/create")
+    public String createProduct() {
+        var product = new Product("Product Test", "This is a test product", 10.00);
+        return productRepository.save(product).toString();
     }
 
 }
